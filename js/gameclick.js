bea2d381204a99f77011926c1dab5de4b5cbdf17
Jokhum <p.werven@gmail.com>
2021-04-16 01:24:29 +0200

    fixing cta buttons

--- js/gameclick.js
+++ js/gameclick.js
@@ -1,15 +1,19 @@
-const gameButton = document.getElementsByClassName("store-item-atc")
+getGames().then(() => {
 
-console.log(gameButton);
 
-    for (var i = 0; i < gameButton.length; i++) {
-
-    const click = gameButton[i]
-
-    click.addEventListener("click", function() {
-
-        window.location.href = "./shoppingcart.html";
-
-    })
-
-}
+    const gameButton = document.getElementsByClassName("store-item-atc")
+    
+    console.log(gameButton);
+    
+        for (var i = 0; i < gameButton.length; i++) {
+    
+        const click = gameButton[i]
+    
+        click.addEventListener("click", function() {
+    
+            window.location.href = "./shoppingcart.html";
+    
+        })
+    
+    }
+    });
--- js/gameinfo.js
+++ js/gameinfo.js
@@ -57,3 +57,23 @@ function createHtml(gameInfo) {
         teamContainer.innerHTML = "An error has occurred."
     }
 }
+
+getGameInfo().then(() => {
+
+
+    const gameButton = document.getElementsByClassName("store-item-atc")
+    
+    console.log(gameButton);
+    
+        for (var i = 0; i < gameButton.length; i++) {
+    
+        const click = gameButton[i]
+    
+        click.addEventListener("click", function() {
+    
+            window.location.href = "./shoppingcart.html";
+    
+        })
+    
+    }
+    });
